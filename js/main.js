92d46fa31f70ccacb2d40419e47427e0f224bc77
seny <senydiaby93@gmail.com>
2021-11-10 16:46:06 +0100

    maj

--- js/main.js
+++ js/main.js
@@ -30,6 +30,33 @@ function permute() {
 //   setInterval(testPermute, 3000);//banniere -> nom de fonction, 3000->intervale
 // }
 
-function modeNuit(){
-    //récuperer body
+let estModeNuit = false;
+function modeNuit() {
+  estModeNuit = !estModeNuit;
+  // alert("dans mode nuit")
+  //récuperer l'élément "body"
+  let body = document.querySelector("body");
+  // document.querySelector(".back");
+  let btn = document.querySelector("#modeNuitBtn");//l'élément bouton
+  let membres = document.querySelectorAll(".member");
+  if (estModeNuit) {
+    body.classList.remove("back");//supprime la classe "back"
+    body.style.backgroundColor = "#000000";
+    btn.innerHTML = "Mode Jour";//on modifie l'etiquette du bouton
+    for (let index = 0; index < membres.length; index++) {
+        const element = membres[index];
+        element.style.backgroundColor="white";
+        
+    }
+  }
+  else {
+    body.classList.add("back");//ajoute la classe "back"
+    btn.innerHTML = "Mode Nuit"; 
+    for (let index = 0; index < membres.length; index++) {
+        const element = membres[index];
+        element.style.backgroundColor="#5BD56F";
+        
+    }
+}
+
 }
